9650bcedb30a35d4de1b20aad049cc6612d57746
Richard Sonderegger <richard@sondinetwork.ch>
2025-10-29 13:24:43 +0100

    Initial Commit

---
+++ DokuSort/DokuSortApp.swift
@@ -0,0 +1,17 @@
+//
+//  DokuSortApp.swift
+//  DokuSort
+//
+//  Created by Richard Sonderegger on 29.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct DokuSortApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
